280ef0048a4146e2aa4ae119a318af5191fd82b6
Luismi Bautista <lmiguelbautista@gmail.com>
2025-04-08 19:12:50 +0200

    Config encryption for PII fields

--- config/application.rb
+++ config/application.rb
@@ -25,5 +25,13 @@ module Hireminator
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.active_record.encryption.primary_key = ENV.fetch("RAILS_ENCRYPTION_PRIMARY_KEY", nil)
+    config.active_record.encryption.deterministic_key = ENV.fetch(
+      "RAILS_ENCRYPTION_DETERMINISTIC_KEY", nil
+    )
+    config.active_record.encryption.key_derivation_salt = ENV.fetch(
+      "RAILS_ENCRYPTION_KEY_DERIVATION_SALT", nil
+    )
   end
 end
