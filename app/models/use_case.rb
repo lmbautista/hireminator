7e1a03cff0e9ca6727e7193c015e6af9e117da69
Luismi Bautista <lmiguelbautista@gmail.com>
2025-04-10 08:43:47 +0200

    Use cases changes
    - Added new constants
    - Added logic to build prompts

--- app/models/use_case.rb
+++ app/models/use_case.rb
@@ -23,6 +23,7 @@ class UseCase < ApplicationRecord
   MODELS = [
     MODEL_GPT4_TURBO = "gpt-4-turbo",
     MODEL_GPT4 = "gpt-4",
+    MODEL_GPT4_MINI = "gpt-4o-mini",
     MODEL_GPT3_5_TURBO = "gpt-3.5-turbo"
   ].freeze
 
@@ -31,6 +32,8 @@ class UseCase < ApplicationRecord
   PROVIDERS = [PROVIDER_OPENAI = "openai", PROVIDER_GEMINI = "gemini"].freeze
 
   enum(:provider, PROVIDERS.index_with { _1 })
+
+  DEFAULT_TEMPERATURE = 0.7
 
   validates :name, presence: true, uniqueness: true
   validates :provider, presence: true, inclusion: { in: PROVIDERS }
@@ -43,4 +46,64 @@ class UseCase < ApplicationRecord
   validates :purpose, presence: true, inclusion: { in: PURPOSES }
   validates :locale, presence: true, inclusion: { in: LOCALES }
   validates :initial_prompt, presence: true
+
+  after_initialize :set_prompts
+
+  def goodbye_key_prompt
+    I18n.t("use_cases.#{purpose}.initial_prompt.goodbye_key", locale:)
+  end
+
+  def full_name
+    return if name.blank?
+
+    [locale.upcase, name].join(" - ")
+  end
+
+  private
+
+  def set_prompts
+    self.name ||= full_name
+    self.initial_prompt ||= build_initial_prompt.as_json
+    self.resume_prompt ||= build_resume_prompt.as_json
+    self.final_prompt ||= build_final_prompt.as_json
+    self.extraction_prompt ||= build_extraction_prompt.as_json
+    self.temperature ||= DEFAULT_TEMPERATURE
+  end
+
+  def build_initial_prompt
+    prompt_key = prompt_for(:initial)
+    system_prompt = I18n.t("#{prompt_key}.system", locale:, inputs: inputs.to_json,
+                                                   outputs: outputs.to_json)
+    user_prompt = I18n.t("#{prompt_key}.user", locale:)
+
+    { system: system_prompt, user: user_prompt }.compact
+  end
+
+  def build_resume_prompt
+    prompt_key = prompt_for(:resume)
+    system_prompt = I18n.t("#{prompt_key}.system", locale:)
+    user_prompt = I18n.t("#{prompt_key}.user", locale:)
+
+    { system: system_prompt, user: user_prompt }.compact
+  end
+
+  def build_final_prompt
+    prompt_key = prompt_for(:final)
+    system_prompt = I18n.t("#{prompt_key}.system", locale:)
+    user_prompt = I18n.t("#{prompt_key}.user", locale:)
+
+    { system: system_prompt, user: user_prompt }.compact
+  end
+
+  def build_extraction_prompt
+    prompt_key = prompt_for(:extraction)
+    system_prompt = I18n.t("#{prompt_key}.system", locale:, outputs: outputs.to_json)
+    user_prompt = I18n.t("#{prompt_key}.user", locale:)
+
+    { system: system_prompt, user: user_prompt }.compact
+  end
+
+  def prompt_for(prompt_type)
+    "use_cases.#{purpose}.#{prompt_type}_prompt"
+  end
 end
--- test/models/use_case_test.rb
+++ test/models/use_case_test.rb
@@ -22,6 +22,7 @@ class UseCaseTest < ActiveSupport::TestCase
 
   test "invalid without initial_prompt" do
     use_case = UseCase.new(purpose: UseCase::PURPOSE_INTERVIEW, name: "Testing interviews")
+    use_case.initial_prompt = nil
 
     assert_not use_case.valid?
     assert use_case.errors.added?(:initial_prompt, :blank)
@@ -62,5 +63,32 @@ class UseCaseTest < ActiveSupport::TestCase
       UseCase.new(name: "Testing interviews", purpose: "unknown",
                   initial_prompt: "Testing random things")
     end
+  end
+
+  test "full_name returns correct value" do
+    use_case = UseCase.new(@valid_attrs)
+    expected_full_name = "EN - Testing interview"
+
+    assert_equal expected_full_name, use_case.send(:full_name)
+  end
+
+  test "set prompts properly" do
+    use_case = UseCase.new(@valid_attrs)
+
+    %w(initial_prompt resume_prompt final_prompt extraction_prompt).each do |prompt_type|
+      prompt = use_case.send(prompt_type.to_sym)
+
+      assert prompt.present?, "Fails for #{prompt_type}"
+      assert prompt["system"].present?, "Fails for #{prompt_type}"
+      assert prompt["user"].present?, "Fails for #{prompt_type}"
+    end
+  end
+
+  test "goodbye_key_prompt" do
+    use_case = UseCase.new(@valid_attrs)
+    expected_prompt = I18n.t("use_cases.#{use_case.purpose}.initial_prompt.goodbye_key",
+                             locale: use_case.locale)
+
+    assert_equal expected_prompt, use_case.goodbye_key_prompt
   end
 end
